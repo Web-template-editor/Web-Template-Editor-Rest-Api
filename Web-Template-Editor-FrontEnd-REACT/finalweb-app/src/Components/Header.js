b466aaf1446bf105cfb6f6377df319d65e1c4788
Arjun <arjuneastkoleri@gmail.com>
2022-01-02 18:25:15 +0530

    drop down linked with db

--- Web-Template-Editor-FrontEnd-REACT/finalweb-app/src/Components/Header.js
+++ Web-Template-Editor-FrontEnd-REACT/finalweb-app/src/Components/Header.js
@@ -1,25 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {Navbar,Nav,NavDropdown}from 'react-bootstrap';
+import axios from "axios";
 
 function Header(){
+  
+  const[project,setProject]=useState([])
 
+  useEffect(()=>{axios.get('http://localhost:8080/projects/project').then((response)=>{
+    console.log(response.data)
+    setProject(response.data)
+  })},[])
 
-        return(
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-
-
+  return(
+  
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    
   <Navbar.Brand href="">WEB-TEMPLATE PROJECT</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="/Details">DETAILS</Nav.Link>
-      
       <NavDropdown title="PROJECT 2020-2022" id="collasible-nav-dropdown">
+        
+        {
+          project.map((obj)=>{
+            return(
+              <NavDropdown.Item href="/Project">{obj.projectName}</NavDropdown.Item>
 
+            )
 
-              <NavDropdown.Item href="/Project">team -1</NavDropdown.Item>
+          })
 
-  
+        }
 
         {/* <NavDropdown.Item href="/Details">team-2</NavDropdown.Item> */}
         
@@ -33,7 +45,6 @@ function Header(){
 
 </Navbar>
 
-        )
-    }
+)}
 
 export default Header;

bda5eab1458d343a6b154ced9fda6ed77508d681
Arjun <arjuneastkoleri@gmail.com>
2022-01-09 12:57:31 +0530

    Revert "project name and description added from db to front end"
    This reverts commit d5f059ec1d15572ce8807618afae38e7ff88b720.

--- Web-Template-Editor-FrontEnd-REACT/finalweb-app/src/Components/Project.js
+++ Web-Template-Editor-FrontEnd-REACT/finalweb-app/src/Components/Project.js
@@ -1,24 +1,20 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './components.css';
 import Details from "./Details";
-function Project (props){
+function Project (){
 
-    const[project,setProject]=useState([])
-    useEffect(()=>{axios.get('http://localhost:8080/projects/project/'+props.match.params.id
-    ).then((response)=>{
-        setProject(response.data)
-        console.log(response.data)
-
-      })},[])
         return(
         
             <div>
 
-            <div className='project'>
+            <div class='project'>
             <br/><br/>
-            <h1>{project.projectName}</h1><br/>
-            <p>{project.projectDescription} </p>
+            <h1>PROJECT 1</h1><br/>
+            <p>You’ve heard the old saying, “practice makes perfect,” right? This situation is the same. Once you gain mastery over the fundamental concepts of React, you must get handsy with building React projects. In the process of developing React projects, you will learn how to develop apps that actually function.
+
+What’s more is that when you start developing React projects, you learn which tools and React ecosystem to use for which React project. The biggest advantage of building apps with React is that once you’ve successfully deployed them, you can incorporate them into your portfolio to impress potential employers.
+
+However, the most tricky part of the process is figuring out what React apps you should build. To help you break the ice with React project development, we’ve compiled a list of React projects for beginners.  </p>
 </div>
 
     <Details />

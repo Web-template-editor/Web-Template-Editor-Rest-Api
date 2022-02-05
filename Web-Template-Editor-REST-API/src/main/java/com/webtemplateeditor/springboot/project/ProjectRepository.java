2e767952b4e8d1bcd4f3f3a3ee25c7166ae51f65
Arjun <arjuneastkoleri@gmail.com>
2022-02-05 21:25:33 +0530

    apis updated

--- Web-Template-Editor-REST-API/src/main/java/com/webtemplateeditor/springboot/project/ProjectRepository.java
+++ Web-Template-Editor-REST-API/src/main/java/com/webtemplateeditor/springboot/project/ProjectRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, String> {
 
-	//@Query("DELETE FROM Project WHERE projectId=:projectid")
-	//HttpStatus deleteByProjectId(String projectId, HttpStatus ok);
+//	@Query("DELETE FROM Project p WHERE p.projectid=:projectId")
+//	HttpStatus deleteByProjectId(String projectId, HttpStatus ok);
 
 }

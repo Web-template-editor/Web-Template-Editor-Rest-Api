d2c9589103fd1de63dface1dc825b2f9e157c52f
26sneharoy <74782706+26sneharoy@users.noreply.github.com>
2022-02-27 12:45:05 +0530

    media

--- Web-Template-Editor-FrontEnd-REACT/finalweb-app/src/Components/Media.js
+++ Web-Template-Editor-FrontEnd-REACT/finalweb-app/src/Components/Media.js
@@ -4,17 +4,19 @@ function Media(){
     return(
         <div className="media container">
         <div class='media'>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyjb6DpFQnpP-Cq5WTbo1ayJu1DUqjDSi85OGFw4uNpDXksh5J7n5pfkFQ9-QmBdQuqTM&usqp=CAU"/>
+        <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/2x/external-user-advertising-kiranshastry-gradient-kiranshastry-4.png"/>
         <h1><b>NAME OF THE MEMBER</b> </h1>
         <p> Hi i am skdjfksd . i did this project during the pandemic time corona 2020. i did my graduation from ksdjkfjsk college and post graduation kfrom tkm engineering college. this was my final year mini project</p>
         <h1><b>MEDIA PROFILES</b></h1>
         <h1></h1>
-        <img src="https://img.icons8.com/fluency/2x/linkedin.png" alt="pic"/><b>linkedin</b>
-        <img src="https://img.icons8.com/color/2x/facebook-new.png" alt="pic"/><b>facebook </b>
-        <img src="https://img.icons8.com/office/2x/whatsapp.png" alt="pic"/><b>whatsapp </b>
-        <img src="https://img.icons8.com/nolan/2x/github.png" alt="pic"/><b>github</b>
-        <img src="https://img.icons8.com/fluency/2x/snapchat.png" alt="pic"/><b>snapchat </b>
-        <img src="https://img.icons8.com/color/2x/twitter.png" alt="pic"/><b>twitter </b>
+        <img src="https://img.icons8.com/nolan/2x/linkedin.png" alt="pic"/><b><a href="https://www.linkedin.com/login">linkedin</a></b>
+        <img src="https://img.icons8.com/nolan/2x/facebook.png" alt="pic"/><b>facebook </b>
+        
+        <img src="https://img.icons8.com/nolan/2x/github.png" alt="pic"/><b><a href="https://github.com/">github</a></b><br/>
+     
+        <img src="https://img.icons8.com/nolan/2x/apple-mail.png" alt="pic"/><b>snehathankomroy@gmail.com </b><br/>
+        <img src="https://img.icons8.com/nolan/2x/apple-phone.png" alt="pic"/><b>+91-7896547889 </b>
+        
         </div>
         
 
